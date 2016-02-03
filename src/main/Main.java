8896d12fb9f3ec4b8d8a816843810557c6ec4c9c
etexpost <etex.post@gmail.com>
2016-02-03 09:55:39 +0200

    push

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,7 +12,11 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../resource/fxml/login.fxml"));
         primaryStage.setTitle("ArkStore");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setMinHeight(350);
+        primaryStage.setMinWidth(350);
+        primaryStage.setMaxHeight(350);
+        primaryStage.setMaxWidth(350);
+        primaryStage.setScene(new Scene(root, 300, 300));
         primaryStage.show();
     }
 
