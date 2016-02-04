85ceaa8a89d380b88afa311d3eb159f347ce1680
etexpost <etex.post@gmail.com>
2016-02-04 20:48:30 +0200

    push

---
+++ src/dao/UserDao.java
@@ -0,0 +1,7 @@
+package dao;
+
+/**
+ * Created by Андрей on 04.02.2016.
+ */
+public interface UserDao {
+}
---
+++ src/dao/daoimpl/UserImpl.java
@@ -0,0 +1,7 @@
+package dao.daoimpl;
+
+/**
+ * Created by Андрей on 04.02.2016.
+ */
+public class UserImpl {
+}
