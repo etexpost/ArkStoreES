656b8869434a6ec21d190ec5628f4a347fb69ed0
etexpost <etex.post@gmail.com>
2016-03-22 00:17:11 +0200

    push

--- src/Controller/Login.java
+++ src/Controller/Login.java
@@ -1,7 +1,33 @@
 package Controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 /**
  * Created by Андрей on 02.02.2016.
  */
 public class Login {
+
+    public void loginWindow() {
+        Stage login = new Stage();
+        Parent root = null;
+        try {
+        root = FXMLLoader.load(getClass().getResource("../resource/fxml/login.fxml"));
+        login.setTitle("ArkStore - Login");
+        login.setScene(new Scene(root, 440, 250));
+        login.setResizable(false);
+        login.initModality(Modality.WINDOW_MODAL);
+
+//        login.initOwner(((Node) actionEvent.getSource()).getScene().getWindow());
+        login.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- src/domain/Main.java
+++ src/domain/Main.java
@@ -1,23 +1,35 @@
 package domain;
 
+import Controller.Login;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.awt.*;
+import java.awt.*;import java.io.IOException;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../resource/fxml/mainWindow.fxml"));
+    public void start(Stage primaryStage) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("../resource/fxml/mainWindow.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         primaryStage.setTitle("ArkStore");
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         primaryStage.setScene(new Scene(root, screenSize.width, screenSize.height));
         primaryStage.setMaximized(true);
         primaryStage.show();
+        Login loginWindow = new Login();
+        ActionEvent actionEvent = new ActionEvent();
+        Login login = new Login();
+        login.loginWindow();
+
     }
 
 
