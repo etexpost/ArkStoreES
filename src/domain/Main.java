34c164484e14dea988d4acb2b7fee4a120fb07e9
etexpost <etex.post@gmail.com>
2016-03-21 20:48:14 +0200

    push

--- src/domain/Main.java
+++ src/domain/Main.java
@@ -6,17 +6,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../resource/fxml/login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../resource/fxml/mainWindow.fxml"));
         primaryStage.setTitle("ArkStore");
-        primaryStage.setMinHeight(350);
-        primaryStage.setMinWidth(450);
-        primaryStage.setMaxHeight(350);
-        primaryStage.setMaxWidth(450);
-        primaryStage.setScene(new Scene(root, 350, 450));
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        primaryStage.setScene(new Scene(root, screenSize.width, screenSize.height));
+        primaryStage.setMaximized(true);
         primaryStage.show();
     }
 
