7c9f51f6e56aa821c01cca12282707f1dd29fbd9
etexpost <etex.post@gmail.com>
2016-04-11 03:36:42 +0300

    push

---
+++ src/Controller/DirClientsAdmin.java
@@ -0,0 +1,14 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ * Created by Андрей on 11.04.2016.
+ */
+
+public class DirClientsAdmin {
+
+    @FXML
+    private Button btnSavechanges;
+}
